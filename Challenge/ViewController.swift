fc743c834a96b8cc6561e9c145681203572873e7
Nathaniel Whittington <whittinn@gmail.com>
2022-06-05 15:40:08 -0500

    Initial Commit

---
+++ Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge
+//
+//  Created by Nathaniel Whittington on 6/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
